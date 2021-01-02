38f598fa13aacb99e6493d99d37786e73c8a6062
yongduosui <yongduosui@gmail.com>
2021-01-03 00:11:22 +0800

    123

---
+++ SS-GCNs/111_run_cora_pruning_acc.sh
@@ -0,0 +1,11 @@
+GPU=$1
+i=0.1
+echo ---------
+echo syd ${i}
+CUDA_VISIBLE_DEVICES=${GPU} \
+python -u main_pruning_acc.py \
+--dataset cora \
+--embedding-dim 1433 16 7 \
+--lr 0.008 \
+--weight-decay 8e-5 \
+--pruning_percent ${i}
