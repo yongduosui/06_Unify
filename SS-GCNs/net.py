189e19f8d0f103f19073ee2f1b7d81badab8c296
yongduosui <yongduosui@gmail.com>
2021-01-02 19:04:49 +0800

    123

--- SS-GCNs/net.py
+++ SS-GCNs/net.py
@@ -4,14 +4,20 @@ import pdb
 
 class net_gcn(nn.Module):
 
-    def __init__(self, embedding_dim, adj):
+    def __init__(self, embedding_dim, adj, load_adj_mask=False):
         super().__init__()
 
         self.layer_num = len(embedding_dim) - 1
         self.net_layer = nn.ModuleList([nn.Linear(embedding_dim[ln], embedding_dim[ln+1], bias=False) for ln in range(self.layer_num)])
         self.relu = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout(p=0.5)
-        self.adj_mask = nn.Parameter(self.generate_adj_mask(adj))
+
+        if load_adj_mask:
+            print("INFO: Load pruning adj mask")
+            self.adj_mask = nn.Parameter(adj)
+        else:
+            print("INFO: Using all ones mask")
+            self.adj_mask = nn.Parameter(self.generate_adj_mask(adj))
 
     def forward(self, x, adj, val_test=False):
 
