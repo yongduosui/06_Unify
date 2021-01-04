ed29544514a593173fdd1648f76781f9f8abfc37
yongduosui <yongduosui@gmail.com>
2021-01-04 21:20:35 +0800

    123

--- SS-GCNs/11_run_cora_pruning.sh
+++ SS-GCNs/11_run_cora_pruning.sh
@@ -1,38 +1,38 @@
 GPU=$1
-i=0.1
-s1=5e-6
-s2=5e-6
-CUDA_VISIBLE_DEVICES=${GPU} \
-python -u main_pruning.py \
---dataset cora \
---embedding-dim 1433 16 7 \
---lr 0.008 \
---weight-decay 8e-5 \
---pruning_percent ${i} \
---total_epoch 300 \
---s1 $s1 \
---s2 $s2
-
 # i=0.1
-# for s1 in 1e-4 5e-5 1e-5 5e-6 1e-6
-# do
-#     for s2 in 5e-4 1e-4 5e-5 1e-5 5e-6 1e-6
-#     do
-#         echo syd ------------------------------------------------------
-#         echo syd s1: $s1 s2: $s2
-#         CUDA_VISIBLE_DEVICES=${GPU} \
-#         python -u main_pruning.py \
-#         --dataset cora \
-#         --embedding-dim 1433 16 7 \
-#         --lr 0.008 \
-#         --weight-decay 8e-5 \
-#         --pruning_percent ${i} \
-#         --total_epoch 300 \
-#         --s1 $s1 \
-#         --s2 $s2
-#         echo syd ------------------------------------------------------
-#     done
-# done
+# s1=5e-6
+# s2=5e-6
+# CUDA_VISIBLE_DEVICES=${GPU} \
+# python -u main_pruning.py \
+# --dataset cora \
+# --embedding-dim 1433 16 7 \
+# --lr 0.008 \
+# --weight-decay 8e-5 \
+# --pruning_percent ${i} \
+# --total_epoch 300 \
+# --s1 $s1 \
+# --s2 $s2
+
+i=0.1
+for s1 in 1e-5 5e-6 1e-6
+do
+    for s2 in 5e-4 1e-4 5e-5 1e-5 5e-6 1e-6
+    do
+        echo syd ------------------------------------------------------
+        echo syd s1: $s1 s2: $s2
+        CUDA_VISIBLE_DEVICES=${GPU} \
+        python -u main_pruning.py \
+        --dataset cora \
+        --embedding-dim 1433 16 7 \
+        --lr 0.008 \
+        --weight-decay 8e-5 \
+        --pruning_percent ${i} \
+        --total_epoch 300 \
+        --s1 $s1 \
+        --s2 $s2
+        echo syd ------------------------------------------------------
+    done
+done
 
 # i=0.1
 # CUDA_VISIBLE_DEVICES=${GPU} \
