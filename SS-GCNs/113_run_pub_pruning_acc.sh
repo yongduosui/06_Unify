73ebbe8c97ad3fb1ba4537225d7f8d31173a27c7
yongduosui <yongduosui@gmail.com>
2021-01-05 18:31:43 +0800

    Create 113_run_pub_pruning_acc.sh

---
+++ SS-GCNs/113_run_pub_pruning_acc.sh
@@ -0,0 +1,11 @@
+GPU=$1
+CUDA_VISIBLE_DEVICES=${GPU} \
+python -u main.py \
+--dataset pubmed \
+--embedding-dim 500 16 3 \
+--lr 0.01 \
+--weight-decay 5e-4 \
+--pruning_percent 0.1 \
+--total_epoch 1000 \
+--s1 1e-5 \
+--s2 1e-5
