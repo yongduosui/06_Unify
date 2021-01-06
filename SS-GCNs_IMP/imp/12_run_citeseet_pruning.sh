d2defb6a691dfeae96cf7d9945e3f1a12d102a31
yongduosui <yongduosui@gmail.com>
2021-01-06 19:29:48 +0800

    123

---
+++ SS-GCNs_IMP/imp/12_run_citeseet_pruning.sh
@@ -0,0 +1,17 @@
+GPU=$1
+i=0.1
+s1=1e-5
+s2=5e-4
+echo syd ------------------------------------------------------
+echo syd s1: $s1 s2: $s2 sp: $i
+CUDA_VISIBLE_DEVICES=${GPU} \
+python -u main_pruning_imp.py \
+--dataset citeseer \
+--embedding-dim 3703 16 6 \
+--lr 0.008 \
+--weight-decay 5e-4 \
+--pruning_percent $i \
+--total_epoch 500 \
+--s1 $s1 \
+--s2 $s2
+echo syd ------------------------------------------------------
