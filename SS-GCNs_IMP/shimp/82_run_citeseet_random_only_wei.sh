009219bf884faa8ac3e5bc666f93f7e8c8b4b277
yongduosui <yongduosui@gmail.com>
2021-01-07 22:43:35 +0800

    213

---
+++ SS-GCNs_IMP/shimp/82_run_citeseet_random_only_wei.sh
@@ -0,0 +1,20 @@
+GPU=$1
+DIM=512
+adj=0
+wei=0.1
+s1=1e-5
+s2=5e-4
+echo syd ------------------------------------------------------
+echo syd s1: $s1 s2: $s2 adj: ${adj} wei: ${wei}
+CUDA_VISIBLE_DEVICES=${GPU} \
+python -u main_pruning_imp.py \
+--dataset citeseer \
+--embedding-dim 3703 ${DIM} 6 \
+--lr 0.01 \
+--weight-decay 5e-4 \
+--pruning_percent_wei ${wei} \
+--pruning_percent_adj ${adj} \
+--total_epoch 500 \
+--s1 $s1 \
+--s2 $s2
+echo syd ------------------------------------------------------
