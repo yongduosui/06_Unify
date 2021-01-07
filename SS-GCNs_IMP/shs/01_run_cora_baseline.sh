66a0f842e9508147404042693a6e8a84bf08764f
yongduosui <yongduosui@gmail.com>
2021-01-07 16:34:50 +0800

    123

--- SS-GCNs_IMP/shs/01_run_cora_baseline.sh
+++ SS-GCNs_IMP/shs/01_run_cora_baseline.sh
@@ -2,6 +2,6 @@ GPU=$1
 CUDA_VISIBLE_DEVICES=${GPU} \
 python -u main.py \
 --dataset cora \
---embedding-dim 1433 16 7 \
+--embedding-dim 1433 512 7 \
 --lr 0.008 \
 --weight-decay 8e-5
--- SS-GCNs_IMP/shs/02_run_citeseet_baseline.sh
+++ SS-GCNs_IMP/shs/02_run_citeseet_baseline.sh
@@ -2,6 +2,6 @@ GPU=$1
 CUDA_VISIBLE_DEVICES=${GPU} \
 python -u main.py \
 --dataset citeseer \
---embedding-dim 3703 16 6 \
+--embedding-dim 3703 512 6 \
 --lr 0.01 \
 --weight-decay 5e-4
