354e1b3176ed16e865cd2cce4313cc1a3cdae4dc
yongduosui <yongduosui@gmail.com>
2021-01-08 21:12:19 +0800

    Update main_pruning_imp.py

--- SS-GCNs_IMP/main_pruning_imp.py
+++ SS-GCNs_IMP/main_pruning_imp.py
@@ -99,7 +99,7 @@ def run_get_mask(args, seed, rewind_weight_mask=None):
             pruning.soft_mask_init(net_gcn, args['init_soft_mask_type'])
         adj_spar, wei_spar = pruning.print_sparsity(net_gcn)
     else:
-        pdb.set_trace()
+        
         pruning.soft_mask_init(net_gcn, args['init_soft_mask_type'])
 
     optimizer = torch.optim.Adam(net_gcn.parameters(), lr=args['lr'], weight_decay=args['weight_decay'])
@@ -172,7 +172,7 @@ if __name__ == "__main__":
     seed = seed_dict[args['dataset']]
     rewind_weight = None
     for p in range(10):
-        
+        pdb.set_trace()
         final_mask_dict, rewind_weight = run_get_mask(args, seed, rewind_weight)
         
         # rewind_weight['adj_mask1_train'] = final_mask_dict['adj_mask']
