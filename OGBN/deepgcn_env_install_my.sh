2047f6b58a6e6aefab69d725bf37dd352ce287b7
yongduosui <yongduosui@gmail.com>
2021-01-10 12:03:53 +0800

    Update deepgcn_env_install_my.sh

--- OGBN/deepgcn_env_install_my.sh
+++ OGBN/deepgcn_env_install_my.sh
@@ -1,16 +1,17 @@
-conda create -n deepgcn python=3.6
+conda create -n deepgcn python=3.7
 source activate deepgcn
 # make sure pytorch version >=1.4.0
-pip install torch==1.6.0 torchvision==0.7.0
+conda install -y pytorch=1.4.0 torchvision cudatoolkit=10.0 python=3.7 -c pytorch
+pip install torch==1.4.0 torchvision==0.5.0
 pip install tensorboard
 
 # command to install pytorch geometric, please refer to the official website for latest installation.
 #  https://pytorch-geometric.readthedocs.io/en/latest/notes/installation.html
 CUDA=cu101
-pip install torch-scatter==2.0.4 -f https://pytorch-geometric.com/whl/torch-1.6.0+${CUDA}.html
-pip install torch-sparse==0.6.1 -f https://pytorch-geometric.com/whl/torch-1.6.0+${CUDA}.html
-pip install torch-spline-conv==1.2.0 -f https://pytorch-geometric.com/whl/torch-1.6.0+${CUDA}.html
-pip install torch-cluster==1.4.5 -f https://pytorch-geometric.com/whl/torch-1.6.0+${CUDA}.html
+pip install torch-scatter==2.0.4 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
+pip install torch-sparse==0.6.1 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
+pip install torch-spline-conv==1.2.0 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
+pip install torch-cluster==1.4.5 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
 pip install torch-geometric==1.4.3
 pip install requests
 
