d82858f4bc7937905f3cad0103a98e9f9248d77a
yongduosui <yongduosui@gmail.com>
2021-01-13 00:09:12 +0800

    Update pruning.py

--- OGBN/unify/ogb/ogbn_arxiv/pruning.py
+++ OGBN/unify/ogb/ogbn_arxiv/pruning.py
@@ -254,7 +254,7 @@ def random_pruning(model, adj_percent, wei_percent):
         pdb.set_trace()
 
         for ii, (ai, wj) in enumerate(wei_pruned_list):
-            model.gcns[ii].mlp[0].weight_mask_train[ai][wj] = 0
+            model.gcns[i].mlp[0].weight_mask_train[ai][wj] = 0
 
         pdb.set_trace()
         model.gcns[i].mlp[0].weight_mask_fixed = model.gcns[i].mlp[0].weight_mask_train
