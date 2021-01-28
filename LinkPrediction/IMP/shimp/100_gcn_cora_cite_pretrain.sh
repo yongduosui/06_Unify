c4c1f2a3141529d5aa8a21faf837fe0e4169b64e
yongduosui <yongduosui@gmail.com>
2021-01-29 00:42:46 +0800

    123

--- LinkPrediction/IMP/shimp/100_gcn_cora_cite_pretrain.sh
+++ LinkPrediction/IMP/shimp/100_gcn_cora_cite_pretrain.sh
@@ -4,12 +4,13 @@ S2=1e-4
 MASK_EPOCH=200
 FIX_EPOCH=200
 CUDA_VISIBLE_DEVICES=$1 \
-python -u main_gcn_imp.py \
+python -u main_gcn_imp_pretrain.py \
 --dataset ${DATASET} \
 --mask_epoch ${MASK_EPOCH} \
 --fix_epoch ${FIX_EPOCH} \
 --s1 ${S1} \
---s2 ${S2}
+--s2 ${S2} \
+--weight_dir ../../GraphCL/cora_double_dgi.pkl
 
 
 DATASET=citeseer
@@ -18,23 +19,10 @@ S2=1e-4
 MASK_EPOCH=200
 FIX_EPOCH=200
 CUDA_VISIBLE_DEVICES=$1 \
-python -u main_gcn_imp.py \
+python -u main_gcn_imp_pretrain.py \
 --dataset ${DATASET} \
 --mask_epoch ${MASK_EPOCH} \
 --fix_epoch ${FIX_EPOCH} \
 --s1 ${S1} \
---s2 ${S2}
-
-
-DATASET=pubmed
-S1=1e-4
-S2=1e-4
-MASK_EPOCH=200
-FIX_EPOCH=200
-CUDA_VISIBLE_DEVICES=$1 \
-python -u main_gcn_imp.py \
---dataset ${DATASET} \
---mask_epoch ${MASK_EPOCH} \
---fix_epoch ${FIX_EPOCH} \
---s1 ${S1} \
---s2 ${S2}
+--s2 ${S2} \
+--weight_dir ../../GraphCL/cite_double_dgi.pkl
