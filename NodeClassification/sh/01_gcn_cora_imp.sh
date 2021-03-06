3ac52303d7b68314f02fc7d9882410ca81f77494
yongduosui <yongduosui@gmail.com>
2021-03-06 16:17:42 +0800

    123

---
+++ NodeClassification/sh/01_gcn_cora_imp.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICE=$1 \
+python -u main_pruning_imp_seed.py \
+--dataset cora \
+--embedding-dim 1433 512 7 \
+--lr 0.008 \
+--weight-decay 8e-5 \
+--pruning_percent_wei 0.2 --pruning_percent_adj 0.05 --total_epoch 200 --s1 1e-2 --s2 1e-2 --init_soft_mask_type all_one
