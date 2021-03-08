acb69be8b76ae05f26810ba3dd6d27e3ba35c33e
yongduosui <yongduosui@gmail.com>
2021-03-08 23:35:35 +0800

    123

--- LinkPrediction/shimp/01_gcn_cora_imp.sh
+++ LinkPrediction/shimp/01_gcn_cora_imp.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=$1 \
-python -u main_gcn_imp_seed.py --dataset cora --mask_epoch 200 --fix_epoch 200 --s1 1e-4 --s2 1e-4
+python -u main_gcn_imp_seed.py --dataset cora --mask_epoch 200 --fix_epoch 200 --s1 1e-3 --s2 1e-4
