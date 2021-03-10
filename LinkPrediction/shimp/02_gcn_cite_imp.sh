392f4e2cc64ae94dcb46339875487929640bfce4
yongduosui <yongduosui@gmail.com>
2021-03-10 10:42:03 +0800

    Update 02_gcn_cite_imp.sh

--- LinkPrediction/shimp/02_gcn_cite_imp.sh
+++ LinkPrediction/shimp/02_gcn_cite_imp.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=$1 \
-python -u main_gcn_imp_seed.py --dataset pubmed --mask_epoch 200 --fix_epoch 200 --s1 1e-3 --s2 1e-3
+python -u main_gcn_imp_seed.py --dataset citeseer --mask_epoch 200 --fix_epoch 200 --s1 1e-3 --s2 1e-3
